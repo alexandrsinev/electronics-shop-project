119d46336c69c5382598f0325c74c2f0923b6088
Alexandr Sinev <sir.sineow@yandex.ru>
2024-01-23 20:00:17 +0300

    Заполнен класс Item для представления товара в магазине, написаны требуемые функции. В test_item.py написаны тесты для класса Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item1():
+    return Item('Смартфон', 10000, 20)
+
+
+@pytest.fixture
+def item2():
+    return Item("Ноутбук", 20000, 5)
+
+
+def test_total_price1(item1):
+    """Проверяем значение возвращаемое методом calculate_total_price для экземпляра item1"""
+    assert item1.calculate_total_price() == 200000
+
+
+def test_total_price2(item2):
+    """Проверяем значение возвращаемое методом calculate_total_price для экземпляра item2"""
+    assert item2.calculate_total_price() == 100000
