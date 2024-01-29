ca3cf78f924186f7e26bd97d0d6d03b06bd49988
Alexandr Sinev <sir.sineow@yandex.ru>
2024-01-30 00:03:13 +0300

    Написаны тесты для методов __repr__ и __str__ и apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -38,3 +38,14 @@ def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+def test_for_repr(item1):
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+def test_for_str(item1):
+    assert str(item1) == 'Смартфон'
+
+def test_apply_discount(item1):
+    item1.pay_rate = 2
+    item1.apply_discount()
+    assert item1.price == 20000
